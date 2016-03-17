e448728bbeb119ae50c48280e04d58a47cc6d9ea
dreameng <1979390719@qq.com>
2016-03-17 16:00:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+__author__ = 'dreameng'
+
+cont = ["a", 'c', 'dd', 'eee', 'a', 'a', 'eee']
+print(cont)
+
+a = u'哈哈哈'
+print len(a)
+print len(u'呵呵')
+
+a = (u'很好', u'hehe', u'haha')
+print a
+
+a = '''foahfahh
+japfhoawhf
+foahvfoah'''
+
+print a
+
+
+
+
